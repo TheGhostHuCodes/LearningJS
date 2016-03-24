e109f83327fcd449c95e852698b416246e7128ad
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-03-23 23:29:16 -0700

    Add babel to dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
-// Gulp dependencies go here.
+const babel = require('gulp-babel');
+
 gulp.task('default', function() {
-  // Gulp tasks go here.
+  // Node source.
+  gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+  // browser source.
+  gulp.src("public/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
 });
